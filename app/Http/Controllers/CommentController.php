469e95ee1175ab6eb278c48c701882cf9a4fba58
de-lolec <matvey20158@mail.ru>
2022-11-08 01:23:54 +0300

    Comment

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\BlogCommentCreateRequest;
+use Illuminate\Http\Request;
+use App\Models\Comment;
+
+class CommentController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(BlogCommentCreateRequest $request)
+    {
+//        $request->validate([
+//            'body'=>'required',
+//        ]);
+
+
+        $input = $request->all();
+        $input['user_id'] = auth()->user()->id;
+
+       // Comment::create($input);
+        $input = (new Comment())->create($input);
+        return back();
+    }
+
+    public function show($id){
+
+    }
+}
